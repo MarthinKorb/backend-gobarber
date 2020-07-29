27b47709afe2d0f08f8e04a6f8c117862edb396a
Marthin Korb <marthinkorb@gmail.com>
2020-07-29 10:18:54 -0300

    Changing importation's name

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -3,13 +3,13 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
-import showProfileService from '@modules/users/services/ShowProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
 export default class ProfileController {
     public async show(request: Request, response: Response): Promise<Response> {
         const user_id = request.user.id;
 
-        const showProfile = container.resolve(showProfileService);
+        const showProfile = container.resolve(ShowProfileService);
         const user = await showProfile.execute({ user_id });
 
         delete user.password;
