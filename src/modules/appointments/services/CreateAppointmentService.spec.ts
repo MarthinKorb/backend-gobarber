14517e290d95f9e0defbd183fd5b401137de8313
Marthin Korb <marthinkorb@gmail.com>
2020-08-01 18:14:43 -0300

    Adding user_id in appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,6 +18,7 @@ describe('CreateAppointment', () => {
     it('should be able to create a new appointment', async () => {
         const appointment = await createAppointment.execute({
             date: new Date(),
+            user_id: '54321',
             provider_id: '123456',
         });
 
@@ -31,12 +32,14 @@ describe('CreateAppointment', () => {
         await createAppointment.execute({
             date: appointmentDate,
             provider_id: '123456',
+            user_id: '54321',
         });
 
         expect(
             createAppointment.execute({
                 date: appointmentDate,
                 provider_id: '123456',
+                user_id: '54321',
             }),
         ).rejects.toBeInstanceOf(AppError);
     });
