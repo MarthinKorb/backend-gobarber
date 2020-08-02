c7724666ed50a585d6bb325645012f109ff5844b
Marthin Korb <marthinkorb@gmail.com>
2020-08-02 15:13:22 -0300

    Adding Validations in Routes with Celebrate

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ForgotPasswordContoller from '../controllers/ForgotPasswordController';
 import ResetPasswordContoller from '../controllers/ResetPasswordController';
 
@@ -7,8 +9,28 @@ const passwordRouter = Router();
 const forgotPasswordContoller = new ForgotPasswordContoller();
 const resetPasswordContoller = new ResetPasswordContoller();
 
-passwordRouter.post('/forgot', forgotPasswordContoller.create);
+passwordRouter.post(
+    '/forgot',
+    celebrate({
+        [Segments.BODY]: {
+            email: Joi.string().email().required(),
+        },
+    }),
+    forgotPasswordContoller.create,
+);
 
-passwordRouter.post('/reset', resetPasswordContoller.create);
+passwordRouter.post(
+    '/reset',
+    celebrate({
+        [Segments.BODY]: {
+            token: Joi.string().uuid().required(),
+            password: Joi.string().required(),
+            password_confirmation: Joi.string()
+                .required()
+                .valid(Joi.ref('password')),
+        },
+    }),
+    resetPasswordContoller.create,
+);
 
 export default passwordRouter;
