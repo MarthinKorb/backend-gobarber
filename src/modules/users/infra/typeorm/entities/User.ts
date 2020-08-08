c1a248e9bd6b2a5b7a5e919d87ae23aae32724a2
Marthin Korb <marthinkorb@gmail.com>
2020-08-08 20:40:03 -0300

    Building config about upload driver in the application

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -6,6 +6,8 @@ import {
     UpdateDateColumn,
 } from 'typeorm';
 
+import uploadConfig from '@config/upload';
+
 import { Exclude, Expose } from 'class-transformer';
 
 @Entity('users')
@@ -34,9 +36,28 @@ class User {
 
     @Expose({ name: 'avatar_url' })
     getavatarUrl(): string | null {
-        return this.avatar
-            ? `${process.env.APP_API_URL}/files/${this.avatar}`
-            : null;
+        if (!this.avatar) {
+            return null;
+        }
+
+        // if (uploadConfig.driver === 'disk') {
+        //     return `${process.env.APP_API_URL}/files/${this.avatar}`;
+        // }
+
+        // if (uploadConfig.driver === 's3') {
+        //     return `https://${uploadConfig.config.aws.bucket}.s3.us-east-2.amazonaws.com/${this.avatar}`;
+        // }
+
+        switch (uploadConfig.driver) {
+            case 'disk':
+                return `${process.env.APP_API_URL}/files/${this.avatar}`;
+
+            case 's3':
+                return `https://${uploadConfig.config.aws.bucket}.s3.us-east-2.amazonaws.com/${this.avatar}`;
+
+            default:
+                return null;
+        }
     }
 }
 
