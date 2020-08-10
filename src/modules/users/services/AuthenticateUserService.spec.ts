abb412fe058c58d7570b5e24cf9c4c97ba16b7dc
Marthin Korb <marthinkorb@gmail.com>
2020-08-09 21:33:13 -0300

    Building and Injecting CacheProvider

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,11 +2,10 @@ import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
-import CreateUserService from './CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashProvider: FakeHashProvider;
-let createUser: CreateUserService;
+
 let authenticateUser: AuthenticateUserService;
 
 describe('AuthenticateUser', () => {
@@ -18,14 +17,10 @@ describe('AuthenticateUser', () => {
             fakeUsersRepository,
             fakeHashProvider,
         );
-        createUser = new CreateUserService(
-            fakeUsersRepository,
-            fakeHashProvider,
-        );
     });
 
     it('should be able to authenticate user', async () => {
-        const user = await createUser.execute({
+        const user = await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'john@exaple.com',
             password: '123456',
@@ -50,7 +45,7 @@ describe('AuthenticateUser', () => {
     });
 
     it('should not be able to authenticate user with wrong password', async () => {
-        await createUser.execute({
+        await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'john@exaple.com',
             password: '123456',
